8d72e635c15cdc4106be29a34e7f1acbafa27c0f
root <root@debian>
2015-09-23 16:21:06 -0400

    redirect

--- borisShell.c
+++ borisShell.c
@@ -8,6 +8,7 @@ using namespace std;
 #include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
  
 /* The array below will hold the arguments: args[0] is the command. */
 static char* args[512];
@@ -31,6 +32,44 @@ int command_pipe[2];
  * So if 'command' returns a file descriptor, the next 'command' has this
  * descriptor as its 'input'.
  */
+
+static void redirect(char** cmd, char**n file){
+  int fds[2];
+  int count;
+  int fd;
+  char c;
+  //pid_t pid;
+
+
+  pipe(fds);
+
+  if (fork() == 0){
+    fd = open(file[0], O_RDONLY);
+
+    if (fd<0){
+      printf("Error: child process 1 redirect");
+      return;
+    }
+    dup2(fds[0],0);
+    close(fds[1]);
+    while ((count = read(0, &c, 1)) > 0){
+      write(fd, &c, 1);
+    }
+  }
+  else if ((pid = fork()) == 0){
+    dup2(fds[1], 1);
+    close(fds[0]);
+    execvp(cmd[0], cmd);
+    perror("execvp failed");
+  }
+  else{
+    waitpid(pid, NULL, 0);
+    close(fds[0]);
+    close(fds[1]);
+  }
+}
+    
+
 static int command(int input, int first, int last)
 {
 	int pipettes[2];
